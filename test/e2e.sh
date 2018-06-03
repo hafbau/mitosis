511dc8fe7e83f8d7cc0a316808229e2f12aa5f3f
Hafiz Suara <hafbau@yahoo.com>
2018-06-03 13:51:26 -0600

    added test script and created core & template folders

---
+++ test/e2e.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# ******************************************************************************
+# This is an end-to-end test intended to run on CI.
+# You can also run it locally but it's slow.
+# ******************************************************************************
+
+function cleanup {
+  echo 'Cleaning up.'
+  cd "$root_path"
+  rm -rf "$temp_app_path"
+}
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
+  echo 'Exiting with error.' 1>&2;
+  exit 1
+}
+
+function handle_exit {
+  cleanup
+  echo 'Exiting without error.' 1>&2;
+  exit
+}
+
+# Check for the existence of one or more files.
+function exists {
+  for f in $*; do
+    test -e "$f"
+  done
+}
+
+# Start in tests/ even if run from root directory
+cd "$(dirname "$0")"
+
+# CLI and app temporary locations
+# http://unix.stackexchange.com/a/84980
+temp_app_path=`mktemp -d 2>/dev/null || mktemp -d -t 'temp_app_path'`
+echo $temp_app_path
+
+# Exit the script with a helpful error message when any error is encountered
+trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
+
+# Cleanup before exit on any termination signal
+trap 'set +x; handle_exit' SIGQUIT SIGTERM SIGINT SIGKILL SIGHUP
+
+# Echo every command being executed
+set -x
+
+# Go to root
+cd ..
+root_path=$PWD
+mitosis="${root_path}/index.js"
+
+# ******************************************************************************
+# Test --scripts-version with a distribution tag
+# ******************************************************************************
+
+cd "$temp_app_path"
+node "$mitosis" test-app
+cd test-app
+
+# Check corresponding top level folders are created.
+exists api
+exists gui
